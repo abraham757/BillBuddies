7a64ba88db2648efd6a6808d58f1770f1eab341d
mcmul <mcmulle20.laxwi@gmail.com>
2025-04-02 16:12:24 -0500

    added mongoosh to try and test mongofb connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,10 +4,11 @@ import dotenv from 'dotenv';
 import './config/connection.js';  // Import the connection file to establish the DB connection
 
 dotenv.config();
+console.log("MongoDB URI:", process.env.MONGODB_URI); // Debugging line
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
