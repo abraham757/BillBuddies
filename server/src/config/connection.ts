30bb22aab41a95bfbd83acf790d1369a4bab22f2
mcmul <mcmulle20.laxwi@gmail.com>
2025-04-02 16:02:55 -0500

    added mongodb enviro var

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -7,8 +7,11 @@ dotenv.config();
 
 const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/billbuddies';
 
+console.log('Attempting to connect to MongoDB...')
+
 mongoose.connect(mongoURI)
-    .then(() => console.log('MongoDB connection established successfully'))
+    .then(() => 
+      console.log('MongoDB connection established successfully'))
     .catch(err => {
       console.error('MongoDB connection error details:', err); 
     });
