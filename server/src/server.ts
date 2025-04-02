4ca708bc0531c4feb03500ff68f5eb3442701fb7
mcmul <mcmulle20.laxwi@gmail.com>
2025-04-02 15:55:57 -0500

    updated server.ts to take in config/connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,10 @@
+//server/server.ts
 import express from 'express';
+import dotenv from 'dotenv';
+import './config/connection.js';  // Import the connection file to establish the DB connection
+
+dotenv.config();
+
 const app = express();
 
 const port = process.env.PORT || 3000;
