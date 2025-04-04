3a90d2b7e8e6616e541063b6e3e37814dbe82c15
mcmul <mcmulle20.laxwi@gmail.com>
2025-04-03 19:45:42 -0500

    moveing

--- server/src/schemas/index.ts
+++ server/src/schemas/index.ts
@@ -1,4 +1,6 @@
-import typeDefs from './typeDefs.js';
+//server/src/schemas/index.ts
+//Compile and export as one
+import typeDefs from './typeDef.js';
 import resolvers from './resolvers.js';
 
 export { typeDefs, resolvers };
--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,46 +1,61 @@
 const typeDefs = `
-type Book {
-  bookId: ID!
-  authors: [String]
-  description: String!
-  title: String!
-  image: String
-  link: String
-}
-  
-type User {
-  _id: ID!
-  username: String!
-  email: String!
-  bookCount: Int
-  savedBooks: [Book]
-}
+  # User type
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    groups: [Group]
+    expensesPaid: [Expense]! # Expenses this user actually paid for
+    expensesCreated: [Expense]! # Expenses this user logged in the system
+  }
 
-type Auth {
-  token: ID!
-  user: User
-}
+  # Group type
+  type Group {
+    _id: ID!
+    groupId: String!
+    members: [User]
+    expenses: [Expense]
+  }
 
-input BookInput {
-  bookId: String!
-  authors: [String]
-  description: String!
-  title: String!
-  image: String
-  link: String
-}
-  
-type Query {
-  me: User
-  books: [Book]  # Add this line to match your resolver
-  user(username: String): User  # Add this line to match your resolver
-}
-    
-type Mutation {
-  login(email: String, username: String, password: String!): Auth
-  addUser(username: String!, email: String!, password: String!): Auth
-  saveBook(bookData: BookInput!): User
-  removeBook(bookId: ID!): User
-}`
+  # Expense type
+  type Expense {
+    _id: ID!
+    amount: Float!
+    description: String!
+    date: String!
+    groupId: Group!
+    createdBy: User! # Who added/logged the expense
+    paidBy User! # Identifies who actually covered the expense
+  }
+
+  # Debt type
+  type Debt {
+    _id: ID!
+    fromUser: User! # Who owes money
+    toUser: User! # Who is owed money
+    amount: Float!
+    expenseId: Expense!
+    settled: Boolean!
+  }
+
+  # Queries
+  type Query {
+    getUser(id: ID!): User
+    getGroup(id: ID!): Group
+    getExpenses(groupId: ID!): [Expense]
+    getDebts(userId: ID!): [Debt]  # Get debts for a specific user
+  }
+
+  # Mutations
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): User
+    createGroup(groupId: String!, members: [ID!]!): Group
+    addExpense(groupId: ID!, paidBy: ID!, amount: Float!, description: String!): Expense #Option add currency
+    splitExpense(expenseId: ID!): [Debt] 
+    settleDebt(debtId: ID!): Debt
+    editExpense(expenseId: ID!, amount: Float!, description: String!): Expense
+    deleteExpense(expenseId: ID!): Expense
+  }
+`;
 
 export default typeDefs;
