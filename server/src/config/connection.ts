977af92e0748d22308806dfff15b87714e896b80
mcmul <mcmulle20.laxwi@gmail.com>
2025-04-03 19:27:58 -0500

    mongodb edit, need to te4st conection

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,16 @@
+// server/config/connection.ts
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+
+const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/billbuddies';
+
+console.log('Attempting to connect to MongoDB...')
+
+mongoose.connect(mongoURI)
+    .then(() => 
+      console.log('MongoDB connection established successfully'))
+    .catch(err => {
+      console.error('MongoDB connection error details:', err); 
+    });
 
 export default mongoose.connection;
